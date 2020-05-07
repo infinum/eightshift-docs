51460cd8f5ed96ba4de004154d02c2bc8a52448d
Kristijan Maric <kristijan.maric@infinum.com>
2020-05-07 10:25:18 +0200

    change image and fix image size

--- website/src/theme/sections/why/index.js
+++ website/src/theme/sections/why/index.js
@@ -12,7 +12,7 @@ function Why() {
         <p className="why__paragraph">Easily use our pre-made 100% customizable existing Gutenberg blocks & components or create your own designs. Browse the collection of useful frontend utility modules and all the tools you need to start building modern Gutenberg dynamic blocks. There’s a bunch of Helpers to write better code (automatic code linting). All code is written following the latest object-oriented programming paradigms.</p>
       </div>
       <div className="why__image">
-        <img src={useBaseUrl('img/img-why-boilerplate.png')} />
+        <img className="why__img" src={useBaseUrl('img/img-why-boilerplate@2x.png')} />
       </div>
     </div>
   </section>
