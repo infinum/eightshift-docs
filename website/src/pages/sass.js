b5666586a72ca38627b45570afc59243bcab5174
Kristijan Maric <kristijan.maric@infinum.com>
2020-05-07 09:06:03 +0200

    add sass and storybook pages

---
+++ website/src/pages/sass.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+
+function Sass() {
+  return (
+    <Layout title="Storybook">
+      <iframe src="https://infinum.github.io/eightshift-frontend-libs/sassdocs/" />
+    </Layout>
+  );
+};
+
+export default Sass;
---
+++ website/src/pages/storybook.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+
+function Storybook() {
+  return (
+    <Layout title="Storybook">
+      <iframe src="https://infinum.github.io/eightshift-frontend-libs/storybook/" />
+    </Layout>
+  );
+};
+
+export default Storybook;
