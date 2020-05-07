e7a491525a19d997f2e43cc492078fe5aa3cd40a
Kristijan Maric <kristijan.maric@infinum.com>
2020-05-07 09:07:57 +0200

    add why boilerplate section and styling

---
+++ website/src/theme/sections/why/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import useBaseUrl from '@docusaurus/useBaseUrl';
+import '../../../scss/sections/why.scss'
+
+function Why() {
+
+  return (
+  <section className="why">
+    <div className="why__container">
+      <div className="why__content">
+        <h2 className="why__title">Why you would<br></br>love to use this?</h2>
+        <p className="why__paragraph">Easily use our pre-made 100% customizable existing Gutenberg blocks & components or create your own designs. Browse the collection of useful frontend utility modules and all the tools you need to start building modern Gutenberg dynamic blocks. There’s a bunch of Helpers to write better code (automatic code linting). All code is written following the latest object-oriented programming paradigms.</p>
+      </div>
+      <div className="why__image">
+        <img src={useBaseUrl('img/img-why-boilerplate.png')} />
+      </div>
+    </div>
+  </section>
+  )
+}
+
+export default Why;
