0560bd59717ec3332ea56d8a6af6edd0e6676feb
Kristijan Maric <kristijan.maric@infinum.com>
2020-05-14 15:06:39 +0200

    add pages

---
+++ src/pages/get-started.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+
+function Start() {
+  return (
+    <Layout>
+    </Layout>
+  );
+}
+
+export default Start;
---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+
+
+function Home() {
+  const context = useDocusaurusContext();
+  const {siteConfig = {}} = context;
+  return (
+    <Layout
+      title={siteConfig.title}
+      description={siteConfig.tagline}>
+    </Layout>
+  );
+}
+
+export default Home;
