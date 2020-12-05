11a7ffb335bfecd43bf03c282fed2007a35fc34b
Ivan Ruzevic <ivan.ruzevic@infinum.hr>
2020-12-05 15:11:33 +0100

    adding component structure chapter

---
+++ website/src/pages/showcase.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import useBaseUrl from '@docusaurus/useBaseUrl';
+import Layout from '@theme/Layout';
+import Iframe from '../theme/sections/iframe/iframe';
+
+export default function Showcase() {
+  const context = useDocusaurusContext();
+  const {siteConfig = {}} = context;
+
+  return (
+    <Layout
+      title={'Showcase'}
+      description={siteConfig.tagline}
+      keywords={siteConfig.customFields.keywords}
+      metaImage={useBaseUrl(`img/${siteConfig.customFields.image}`)}
+    >
+
+    </Layout>
+  );
+};
