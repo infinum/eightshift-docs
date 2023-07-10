675e3ebd21332efc6a37078c47654fdaf11594f9
Goran Alković <77000136+goranalkovic-infinum@users.noreply.github.com>
2023-07-10 10:01:33 +0200

    rework showcase pages

--- website/src/pages/showcase-infinum.js
+++ website/src/pages/showcase-infinum.js
@@ -3,8 +3,6 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 import Layout from '@theme/Layout';
 import ShowcaseGrid from '../theme/sections/showcase-grid/showcase-grid';
-import Footer from '../theme/sections/footer/footer';
-import Foot from '../theme/sections/foot/foot';
 
 export default function Showcase() {
 	const context = useDocusaurusContext();
@@ -12,15 +10,12 @@ export default function Showcase() {
 
 	return (
 		<Layout
-			title={'Showcase Infinum'}
+			title='Infinum showcase'
 			description={siteConfig.tagline}
 			keywords={siteConfig.customFields.keywords}
 			metaImage={useBaseUrl(`img/${siteConfig.customFields.image}`)}
-			wrapperClassName={'page-showcase'}
 		>
 			<ShowcaseGrid privateType={true}/>
-			<Footer />
-			<Foot />
 		</Layout>
 	);
 };
