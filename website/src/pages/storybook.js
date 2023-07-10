89db01e08a88fbddeebb934177fbcb193f05494e
Goran Alković <77000136+goranalkovic-infinum@users.noreply.github.com>
2023-07-10 10:01:12 +0200

    rework storybook and sass pages

--- website/src/pages/storybook.js
+++ website/src/pages/storybook.js
@@ -2,23 +2,20 @@ import React from 'react';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 import Layout from '@theme/Layout';
-import Iframe from '../theme/sections/iframe/iframe';
 
 export default function Storybook() {
 	const context = useDocusaurusContext();
-	const {siteConfig = {}} = context;
+	const { siteConfig = {} } = context;
 
 	return (
 		<Layout
-			title={'Storybook'}
+			title='Storybook'
 			description={siteConfig.tagline}
 			keywords={siteConfig.customFields.keywords}
 			metaImage={useBaseUrl(`img/${siteConfig.customFields.image}`)}
-			wrapperClassName={'page-storybook'}
+			wrapperClassName='es-single-full-screen-child'
 		>
-			<Iframe
-				src="https://infinum.github.io/eightshift-frontend-libs/storybook/"
-			/>
+			<iframe className='es-full-size' src='https://infinum.github.io/eightshift-frontend-libs/storybook/' />
 		</Layout>
 	);
 };
