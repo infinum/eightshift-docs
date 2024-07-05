2989f93e9300b195459057bd03b3a8104619b1f9
Goran Alković <77000136+goranalkovic-infinum@users.noreply.github.com>
2024-07-05 08:49:00 +0200

    fix docusaurus build

--- website/ui-components/components/component-showcase.js
+++ website/ui-components/components/component-showcase.js
@@ -1,6 +1,7 @@
 import { useState, useRef } from 'react';
 import { Button } from '@eightshift/ui-components';
 import { clsx } from 'clsx';
+import BrowserOnly from '@docusaurus/BrowserOnly';
 
 export const ComponentShowcase = ({
 	children,
@@ -14,30 +15,34 @@ export const ComponentShowcase = ({
 	const ref = useRef();
 
 	return (
-		<div className='es-uic-flex es-uic-font-sans'>
-			<div
-				className={clsx(
-					'es-uic-border es-uic-border-dashed es-uic-border-gray-200 es-uic-p-4 es-uic-rounded-lg es-uic-space-y-2.5 es-uic-shrink-0',
-					fitWidth ? 'es-uic-w-fit' : 'es-uic-w-96',
-					className
-				)}
-			>
-				{typeof children === 'function' && children(data, setData)}
-				{typeof children !== 'function' && children}
-			</div>
-
-			<div className='es-uic-space-y-2.5 es-uic-ml-3 es-uic-grow'>
-				{preContent && preContent(data, setData, ref)}
-				{resettable && (
-					<Button
-						size='small'
-						onPress={() => setData(defaultValue)}
-						disabled={data === defaultValue}
+		<BrowserOnly>
+			{() => (
+				<div className='es-uic-flex es-uic-font-sans'>
+					<div
+						className={clsx(
+							'es-uic-border es-uic-border-dashed es-uic-border-gray-200 es-uic-p-4 es-uic-rounded-lg es-uic-space-y-2.5 es-uic-shrink-0',
+							fitWidth ? 'es-uic-w-fit' : 'es-uic-w-96',
+							className
+						)}
 					>
-						Reset
-					</Button>
-				)}
-			</div>
-		</div>
+						{typeof children === 'function' && children(data, setData)}
+						{typeof children !== 'function' && children}
+					</div>
+
+					<div className='es-uic-space-y-2.5 es-uic-ml-3 es-uic-grow'>
+						{preContent && preContent(data, setData, ref)}
+						{resettable && (
+							<Button
+								size='small'
+								onPress={() => setData(defaultValue)}
+								disabled={data === defaultValue}
+							>
+								Reset
+							</Button>
+						)}
+					</div>
+				</div>
+			)}
+		</BrowserOnly>
 	);
 };
