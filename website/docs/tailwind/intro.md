e12d6c9cedd897f94b066769f1f3598264ebf690
Goran Alković <77000136+goranalkovic-infinum@users.noreply.github.com>
2024-07-10 12:38:27 +0200

    add tailwind docs

---
+++ website/docs/tailwind/intro.md
@@ -0,0 +1,10 @@
+---
+id: intro
+title: Introduction
+---
+
+[TailwindCSS](https://tailwindcss.com/) is a utility-first CSS framework for rapidly building custom user interfaces. It provides a set of utility classes that you can use for styling, without having to write custom CSS. This speeds up the development process, makes everything more consistent and maintainable, while keeping the final output light.
+
+:::note
+🚧 Work in progress, documentation will be expanded as time goes.
+:::
--- website/sidebars.js
+++ website/sidebars.js
@@ -9,14 +9,14 @@ module.exports = {
 			'theme',
 			'plugin',
 		],
-		'Basics': [
+		Basics: [
 			'basics/basics-intro',
 			'basics/wp-cli',
 			'basics/architecture-concepts',
 			{
-				'type': 'category',
-				'label': 'Backend',
-				'items': [
+				type: 'category',
+				label: 'Backend',
+				items: [
 					'basics/backend',
 					'basics/namespaces',
 					'basics/extending-classes',
@@ -24,20 +24,16 @@ module.exports = {
 					'basics/example-class',
 					'basics/manifest',
 					{
-						'type': 'category',
-						'label': 'Rest',
-						'items': [
-							'basics/rest',
-							'basics/rest-field',
-							'basics/rest-route',
-						]
+						type: 'category',
+						label: 'Rest',
+						items: ['basics/rest', 'basics/rest-field', 'basics/rest-route'],
 					},
-				]
+				],
 			},
 			{
-				'type': 'category',
-				'label': 'Frontend',
-				'items': [
+				type: 'category',
+				label: 'Frontend',
+				items: [
 					'basics/frontend',
 					'basics/webpack',
 					'basics/library',
@@ -46,12 +42,12 @@ module.exports = {
 					'basics/fonts',
 					'basics/browser-sync',
 					'basics/dynamic-import',
-				]
+				],
 			},
 			{
-				'type': 'category',
-				'label': 'Blocks',
-				'items': [
+				type: 'category',
+				label: 'Blocks',
+				items: [
 					'basics/blocks',
 					'basics/blocks-important',
 					'basics/blocks-faq',
@@ -73,24 +69,22 @@ module.exports = {
 					'basics/blocks-reusable',
 					'basics/blocks-styles',
 					'basics/blocks-storybook',
-				]
+				],
 			},
 			{
-				'type': 'category',
-				'label': 'Helpers',
-				'items': [
-					'basics/helpers',
-					'basics/helpers-javascript',
-					'basics/helpers-scss',
-					'basics/helpers-php',
-				]
+				type: 'category',
+				label: 'Helpers',
+				items: ['basics/helpers', 'basics/helpers-javascript', 'basics/helpers-scss', 'basics/helpers-php'],
 			},
 			'basics/tips-tricks',
+			{
+				type: 'category',
+				label: 'TailwindCSS',
+				items: ['tailwind/intro', 'tailwind/getting-started', 'tailwind/manifest', 'tailwind/helpers'],
+			},
 		],
-		'Tutorials': [
-			'tutorials/intro',
-		],
-		'Migrations': [
+		Tutorials: ['tutorials/intro'],
+		Migrations: [
 			'migrations/11-12',
 			'migrations/10-11',
 			'migrations/9-10',
@@ -110,23 +104,23 @@ module.exports = {
 			'additional-libraries/storybook',
 			'additional-libraries/stubs',
 		],
-		'Legacy': [
+		Legacy: [
 			{
-				'type': 'category',
-				'label': 'v8',
-				'items': [
+				type: 'category',
+				label: 'v8',
+				items: [
 					'legacy/v8/versions',
 					{
-						'type': 'category',
-						'label': 'Basics',
-						'items': [
+						type: 'category',
+						label: 'Basics',
+						items: [
 							'legacy/v8/basics/basics-intro',
 							'legacy/v8/basics/wp-cli',
 							'legacy/v8/basics/architecture-concepts',
 							{
-								'type': 'category',
-								'label': 'Backend',
-								'items': [
+								type: 'category',
+								label: 'Backend',
+								items: [
 									'legacy/v8/basics/backend',
 									'legacy/v8/basics/namespaces',
 									'legacy/v8/basics/extending-classes',
@@ -134,20 +128,16 @@ module.exports = {
 									'legacy/v8/basics/example-class',
 									'legacy/v8/basics/manifest',
 									{
-										'type': 'category',
-										'label': 'Rest',
-										'items': [
-											'legacy/v8/basics/rest',
-											'legacy/v8/basics/rest-field',
-											'legacy/v8/basics/rest-route',
-										]
+										type: 'category',
+										label: 'Rest',
+										items: ['legacy/v8/basics/rest', 'legacy/v8/basics/rest-field', 'legacy/v8/basics/rest-route'],
 									},
-								]
+								],
 							},
 							{
-								'type': 'category',
-								'label': 'Frontend',
-								'items': [
+								type: 'category',
+								label: 'Frontend',
+								items: [
 									'legacy/v8/basics/frontend',
 									'legacy/v8/basics/webpack',
 									'legacy/v8/basics/library',
@@ -156,12 +146,12 @@ module.exports = {
 									'legacy/v8/basics/fonts',
 									'legacy/v8/basics/browser-sync',
 									'legacy/v8/basics/dynamic-import',
-								]
+								],
 							},
 							{
-								'type': 'category',
-								'label': 'Blocks',
-								'items': [
+								type: 'category',
+								label: 'Blocks',
+								items: [
 									'legacy/v8/basics/blocks',
 									'legacy/v8/basics/blocks-important',
 									'legacy/v8/basics/blocks-faq',
@@ -183,39 +173,39 @@ module.exports = {
 									'legacy/v8/basics/blocks-reusable',
 									'legacy/v8/basics/blocks-styles',
 									'legacy/v8/basics/blocks-storybook',
-								]
+								],
 							},
 							{
-								'type': 'category',
-								'label': 'Helpers',
-								'items': [
+								type: 'category',
+								label: 'Helpers',
+								items: [
 									'legacy/v8/basics/helpers',
 									'legacy/v8/basics/helpers-javascript',
 									'legacy/v8/basics/helpers-scss',
 									'legacy/v8/basics/helpers-php',
-								]
+								],
 							},
 							'legacy/v8/basics/tips-tricks',
 						],
-					}
-				]
+					},
+				],
 			},
 			{
-				'type': 'category',
-				'label': 'v7',
-				'items': [
+				type: 'category',
+				label: 'v7',
+				items: [
 					'legacy/v7/versions',
 					{
-						'type': 'category',
-						'label': 'Basics',
-						'items': [
+						type: 'category',
+						label: 'Basics',
+						items: [
 							'legacy/v7/basics/basics-intro',
 							'legacy/v7/basics/wp-cli',
 							'legacy/v7/basics/architecture-concepts',
 							{
-								'type': 'category',
-								'label': 'Backend',
-								'items': [
+								type: 'category',
+								label: 'Backend',
+								items: [
 									'legacy/v7/basics/backend',
 									'legacy/v7/basics/namespaces',
 									'legacy/v7/basics/extending-classes',
@@ -223,20 +213,16 @@ module.exports = {
 									'legacy/v7/basics/example-class',
 									'legacy/v7/basics/manifest',
 									{
-										'type': 'category',
-										'label': 'Rest',
-										'items': [
-											'legacy/v7/basics/rest',
-											'legacy/v7/basics/rest-field',
-											'legacy/v7/basics/rest-route',
-										]
+										type: 'category',
+										label: 'Rest',
+										items: ['legacy/v7/basics/rest', 'legacy/v7/basics/rest-field', 'legacy/v7/basics/rest-route'],
 									},
-								]
+								],
 							},
 							{
-								'type': 'category',
-								'label': 'Frontend',
-								'items': [
+								type: 'category',
+								label: 'Frontend',
+								items: [
 									'legacy/v7/basics/frontend',
 									'legacy/v7/basics/webpack',
 									'legacy/v7/basics/library',
@@ -245,12 +231,12 @@ module.exports = {
 									'legacy/v7/basics/fonts',
 									'legacy/v7/basics/browser-sync',
 									'legacy/v7/basics/dynamic-import',
-								]
+								],
 							},
 							{
-								'type': 'category',
-								'label': 'Blocks',
-								'items': [
+								type: 'category',
+								label: 'Blocks',
+								items: [
 									'legacy/v7/basics/blocks',
 									'legacy/v7/basics/blocks-important',
 									'legacy/v7/basics/blocks-faq',
@@ -272,39 +258,39 @@ module.exports = {
 									'legacy/v7/basics/blocks-reusable',
 									'legacy/v7/basics/blocks-styles',
 									'legacy/v7/basics/blocks-storybook',
-								]
+								],
 							},
 							{
-								'type': 'category',
-								'label': 'Helpers',
-								'items': [
+								type: 'category',
+								label: 'Helpers',
+								items: [
 									'legacy/v7/basics/helpers',
 									'legacy/v7/basics/helpers-javascript',
 									'legacy/v7/basics/helpers-scss',
 									'legacy/v7/basics/helpers-php',
-								]
+								],
 							},
 							'legacy/v7/basics/tips-tricks',
 						],
-					}
-				]
+					},
+				],
 			},
 			{
-				'type': 'category',
-				'label': 'v6',
-				'items': [
+				type: 'category',
+				label: 'v6',
+				items: [
 					'legacy/v6/versions',
 					{
-						'type': 'category',
-						'label': 'Basics',
-						'items': [
+						type: 'category',
+						label: 'Basics',
+						items: [
 							'legacy/v6/basics/basics-intro',
 							'legacy/v6/basics/wp-cli',
 							'legacy/v6/basics/architecture-concepts',
 							{
-								'type': 'category',
-								'label': 'Backend',
-								'items': [
+								type: 'category',
+								label: 'Backend',
+								items: [
 									'legacy/v6/basics/backend',
 									'legacy/v6/basics/namespaces',
 									'legacy/v6/basics/extending-classes',
@@ -312,20 +298,16 @@ module.exports = {
 									'legacy/v6/basics/example-class',
 									'legacy/v6/basics/manifest',
 									{
-										'type': 'category',
-										'label': 'Rest',
-										'items': [
-											'legacy/v6/basics/rest',
-											'legacy/v6/basics/rest-field',
-											'legacy/v6/basics/rest-route',
-										]
+										type: 'category',
+										label: 'Rest',
+										items: ['legacy/v6/basics/rest', 'legacy/v6/basics/rest-field', 'legacy/v6/basics/rest-route'],
 									},
-								]
+								],
 							},
 							{
-								'type': 'category',
-								'label': 'Frontend',
-								'items': [
+								type: 'category',
+								label: 'Frontend',
+								items: [
 									'legacy/v6/basics/frontend',
 									'legacy/v6/basics/webpack',
 									'legacy/v6/basics/library',
@@ -334,12 +316,12 @@ module.exports = {
 									'legacy/v6/basics/fonts',
 									'legacy/v6/basics/browser-sync',
 									'legacy/v6/basics/dynamic-import',
-								]
+								],
 							},
 							{
-								'type': 'category',
-								'label': 'Blocks',
-								'items': [
+								type: 'category',
+								label: 'Blocks',
+								items: [
 									'legacy/v6/basics/blocks',
 									'legacy/v6/basics/blocks-important',
 									'legacy/v6/basics/blocks-faq',
@@ -361,38 +343,38 @@ module.exports = {
 									'legacy/v6/basics/blocks-reusable',
 									'legacy/v6/basics/blocks-styles',
 									'legacy/v6/basics/blocks-storybook',
-								]
+								],
 							},
 							{
-								'type': 'category',
-								'label': 'Helpers',
-								'items': [
+								type: 'category',
+								label: 'Helpers',
+								items: [
 									'legacy/v6/basics/helpers',
 									'legacy/v6/basics/helpers-javascript',
 									'legacy/v6/basics/helpers-scss',
 									'legacy/v6/basics/helpers-php',
-								]
+								],
 							},
 							'legacy/v6/basics/tips-tricks',
 						],
-					}
+					},
 				],
 			},
 			{
-				'type': 'category',
-				'label': 'v5',
-				'items': [
+				type: 'category',
+				label: 'v5',
+				items: [
 					'legacy/v5/versions',
 					{
-						'type': 'category',
-						'label': 'Basics',
-						'items': [
+						type: 'category',
+						label: 'Basics',
+						items: [
 							'legacy/v5/basics/basics-intro',
 							'legacy/v5/basics/wp-cli',
 							{
-								'type': 'category',
-								'label': 'Backend Stuff',
-								'items': [
+								type: 'category',
+								label: 'Backend Stuff',
+								items: [
 									'legacy/v5/basics/backend',
 									'legacy/v5/basics/namespaces',
 									'legacy/v5/basics/extending-classes',
@@ -401,20 +383,16 @@ module.exports = {
 									'legacy/v5/basics/the-structure',
 									'legacy/v5/basics/manifest',
 									{
-										'type': 'category',
-										'label': 'Rest',
-										'items': [
-											'legacy/v5/basics/rest',
-											'legacy/v5/basics/rest-field',
-											'legacy/v5/basics/rest-route',
-										]
+										type: 'category',
+										label: 'Rest',
+										items: ['legacy/v5/basics/rest', 'legacy/v5/basics/rest-field', 'legacy/v5/basics/rest-route'],
 									},
-								]
+								],
 							},
 							{
-								'type': 'category',
-								'label': 'Frontend Stuff',
-								'items': [
+								type: 'category',
+								label: 'Frontend Stuff',
+								items: [
 									'legacy/v5/basics/frontend',
 									'legacy/v5/basics/webpack',
 									'legacy/v5/basics/library',
@@ -423,12 +401,12 @@ module.exports = {
 									'legacy/v5/basics/fonts',
 									'legacy/v5/basics/browser-sync',
 									'legacy/v5/basics/dynamic-import',
-								]
+								],
 							},
 							{
-								'type': 'category',
-								'label': 'Blocks',
-								'items': [
+								type: 'category',
+								label: 'Blocks',
+								items: [
 									'legacy/v5/basics/blocks',
 									'legacy/v5/basics/blocks-important',
 									'legacy/v5/basics/blocks-intro',
@@ -450,31 +428,31 @@ module.exports = {
 									'legacy/v5/basics/blocks-reusable',
 									'legacy/v5/basics/blocks-styles',
 									'legacy/v5/basics/blocks-storybook',
-								]
+								],
 							},
 							{
-								'type': 'category',
-								'label': 'Helpers',
-								'items': [
+								type: 'category',
+								label: 'Helpers',
+								items: [
 									'legacy/v5/basics/helpers',
 									'legacy/v5/basics/helpers-javascript',
 									'legacy/v5/basics/helpers-scss',
 									'legacy/v5/basics/helpers-php',
-								]
+								],
 							},
-						]
+						],
 					},
-				]
+				],
 			},
 			{
-				'type': 'category',
-				'label': 'v4',
-				'items': [
+				type: 'category',
+				label: 'v4',
+				items: [
 					'legacy/v4/versions',
 					{
-						'type': 'category',
-						'label': 'Guides',
-						'items': [
+						type: 'category',
+						label: 'Guides',
+						items: [
 							'legacy/v4/guides/extending-classes',
 							'legacy/v4/guides/di-container',
 							'legacy/v4/guides/config',
@@ -483,9 +461,9 @@ module.exports = {
 							'legacy/v4/guides/post-type',
 							'legacy/v4/guides/taxonomy',
 							{
-								'type': 'category',
-								'label': 'Blocks',
-								'items': [
+								type: 'category',
+								label: 'Blocks',
+								items: [
 									'legacy/v4/guides/blocks-registration',
 									'legacy/v4/guides/blocks-structure',
 									'legacy/v4/guides/blocks-structure-block-item',
@@ -494,74 +472,66 @@ module.exports = {
 									'legacy/v4/guides/blocks-block-from-components',
 									'legacy/v4/guides/blocks-render-block-view-helper',
 									'legacy/v4/guides/blocks-get-actions-helper',
-									'legacy/v4/guides/blocks-filter-block-attributes-override'
-								]
+									'legacy/v4/guides/blocks-filter-block-attributes-override',
+								],
 							},
 							'legacy/v4/guides/i18n',
 							'legacy/v4/guides/media',
 							'legacy/v4/guides/menu',
 							{
-								'type': 'category',
-								'label': 'Rest API',
-								'items': [
+								type: 'category',
+								label: 'Rest API',
+								items: [
 									'legacy/v4/guides/rest-intro',
 									'legacy/v4/guides/rest-field-example',
-									'legacy/v4/guides/rest-route-example'
-								]
+									'legacy/v4/guides/rest-route-example',
+								],
 							},
 							{
-								'type': 'category',
-								'label': 'Listing Columns',
-								'items': [
+								type: 'category',
+								label: 'Listing Columns',
+								items: [
 									'legacy/v4/guides/columns-post-type',
 									'legacy/v4/guides/columns-taxonomy',
-									'legacy/v4/guides/columns-user'
-								]
+									'legacy/v4/guides/columns-user',
+								],
 							},
 							'legacy/v4/guides/login',
 							{
-								'type': 'category',
-								'label': 'Fonts',
-								'items': [
-									'legacy/v4/guides/fonts-custom',
-									'legacy/v4/guides/fonts-icon'
-								]
-							}
+								type: 'category',
+								label: 'Fonts',
+								items: ['legacy/v4/guides/fonts-custom', 'legacy/v4/guides/fonts-icon'],
+							},
 						],
 					},
 					{
-						'type': 'category',
-						'label': 'Advanced',
-						'items': [
+						type: 'category',
+						label: 'Advanced',
+						items: [
 							'legacy/v4/advanced/docs-sass',
 							'legacy/v4/advanced/webpack',
 							'legacy/v4/advanced/browsersync',
 							{
-								'type': 'category',
-								'label': 'Helpers',
-								'items': [
+								type: 'category',
+								label: 'Helpers',
+								items: [
 									'legacy/v4/advanced/helpers-aliases-helpers',
 									'legacy/v4/advanced/helpers-object-helpers',
 									'legacy/v4/advanced/helpers-components-helpers',
 									'legacy/v4/advanced/helpers-shortcode-helpers',
-									'legacy/v4/advanced/helpers-error-logger-helpers'
-								]
+									'legacy/v4/advanced/helpers-error-logger-helpers',
+								],
 							},
 							{
-								'type': 'category',
-								'label': 'Custom Components',
-								'items': [
-									'legacy/v4/advanced/components-color-palette',
-									'legacy/v4/advanced/components-heading-level'
-								]
+								type: 'category',
+								label: 'Custom Components',
+								items: ['legacy/v4/advanced/components-color-palette', 'legacy/v4/advanced/components-heading-level'],
 							},
-						]
+						],
 					},
-				]
+				],
 			},
 		],
-		'Crash Course': [
-			'crash-course/intro',
-		],
+		'Crash Course': ['crash-course/intro'],
 	},
 };
